0d512f94077299666d1b1fa10af674adbf798d44
OberoiSahaj <oberoi_sahaj@yahoo.com>
2018-07-04 10:53:40 +0530

    Error Handling basic code:)

---
+++ handling.py
@@ -0,0 +1,20 @@
+try:
+    f = open("ab.txt")
+    a= 10
+    b= 0
+    print(a/b)
+    for line in f:
+        print(line)
+
+#except (FileNotFoundError, ZeroDivisionError) :      #   this gives us the error name-   except Exception as e:
+ #   print("Unable to find file")                                                        #print(e)
+
+except ZeroDivisionError:
+    print("Zero division error bro")
+except FileNotFoundError:
+    print("file not foundd")
+#write print(dir(e)) to find what all e can show.
+
+# i= 0/0
+
+
